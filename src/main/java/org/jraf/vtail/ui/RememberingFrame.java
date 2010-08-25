8df0bf132ceb16ca894740bde0f6f7e5a1fb54f0
BoD <BoD@JRAF.org>
2010-08-25 19:40:37 +0200

    Trying to fix full screen problem

--- src/main/java/org/jraf/vtail/ui/RememberingFrame.java
+++ src/main/java/org/jraf/vtail/ui/RememberingFrame.java
@@ -33,7 +33,8 @@ import javax.swing.JFrame;
 import org.jraf.vtail.misc.Config;
 import org.jraf.vtail.misc.Log;
 
-public class RememberingFrame extends JFrame {
+public class RememberingFrame extends JFrame
+{
     private static final String TAG = RememberingFrame.class.getName();
 
     private static final long serialVersionUID = -8884339290080349855L;
@@ -44,20 +45,26 @@ public class RememberingFrame extends JFrame {
     private static final String PREF_X = "x";
     private static final String PREF_Y = "y";
 
-
     private final Preferences mPreferences;
 
-    public RememberingFrame(final Class clazz) {
+    public RememberingFrame(final Class clazz)
+    {
         super();
         mPreferences = Preferences.userNodeForPackage(clazz).node(clazz.getSimpleName());
-        addComponentListener(new ComponentAdapter() {
+        addComponentListener(new ComponentAdapter()
+        {
             @Override
-            public void componentResized(final ComponentEvent componentEvent) {
+            public void componentResized(final ComponentEvent componentEvent)
+            {
                 final boolean maximized = (getExtendedState() & MAXIMIZED_BOTH) == MAXIMIZED_BOTH;
-                if (Config.LOGD) Log.d(TAG, "maximized=" + maximized + " getExtendedState()=" + getExtendedState());
-                if (maximized) {
+                if (Config.LOGD) Log.d(TAG, "componentResized maximized=" + maximized + " getExtendedState()="
+                        + getExtendedState());
+                if (maximized)
+                {
                     mPreferences.put(PREF_MAXIMIZED, "true");
-                } else {
+                }
+                else
+                {
                     mPreferences.put(PREF_MAXIMIZED, "false");
                     mPreferences.put(PREF_WIDTH, "" + getWidth());
                     mPreferences.put(PREF_HEIGHT, "" + getHeight());
@@ -66,9 +73,12 @@ public class RememberingFrame extends JFrame {
             }
 
             @Override
-            public void componentMoved(final ComponentEvent componentEvent) {
+            public void componentMoved(final ComponentEvent componentEvent)
+            {
                 final boolean maximized = (getExtendedState() & MAXIMIZED_BOTH) == MAXIMIZED_BOTH;
-                if (!maximized) {
+                if (Config.LOGD) Log.d(TAG, "componentMoved maximized=" + maximized);
+                if (!maximized)
+                {
                     mPreferences.put(PREF_X, "" + getX());
                     mPreferences.put(PREF_Y, "" + getY());
                 }
@@ -77,14 +87,25 @@ public class RememberingFrame extends JFrame {
     }
 
     @Override
-    public void pack() {
-        if (mPreferences.get(PREF_X, null) == null) {
+    public void pack()
+    {
+        boolean maximize = "true".equals(mPreferences.get(PREF_MAXIMIZED, "false"));
+        if (Config.LOGD) Log.d(TAG, "pref maximized=" + maximize);
+        if (mPreferences.get(PREF_X, null) == null)
+        {
             setSize(320, 200);
             setLocationRelativeTo(null);
-        } else {
-            setBounds(Integer.parseInt(mPreferences.get(PREF_X, "320")), Integer.parseInt(mPreferences.get(PREF_Y, "240")),
-                    Integer.parseInt(mPreferences.get(PREF_WIDTH, "320")), Integer.parseInt(mPreferences.get(PREF_HEIGHT, "240")));
-            if ("true".equals(mPreferences.get(PREF_MAXIMIZED, "false"))) {
+        }
+        else
+        {
+            if (Config.LOGD) Log.d(TAG, "setBounds");
+            setBounds(Integer.parseInt(mPreferences.get(PREF_X, "320")),
+                    Integer.parseInt(mPreferences.get(PREF_Y, "240")),
+                    Integer.parseInt(mPreferences.get(PREF_WIDTH, "320")),
+                    Integer.parseInt(mPreferences.get(PREF_HEIGHT, "240")));
+            if (maximize)
+            {
+                if (Config.LOGD) Log.d(TAG, "setExtendedState");
                 setExtendedState(MAXIMIZED_BOTH);
             }
         }
